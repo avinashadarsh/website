8bf4084f0bd1f6e1a094722fe78cfafa8427cf0a
darrell couch <darrellneilcouch@gmail.com>
2019-11-15 00:15:22 -0700

    added angular again

---
+++ clientApp/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Toolbar } from './layout/toolbar/toolbar.component';
+
+import {MatSidenavModule, MatButtonModule, MatIconModule} from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Main } from './layout/main/main.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ContentModule } from './content/content.module';
+
+const routes : Routes = [
+  {path: "", loadChildren: "./content/content.module#ContentModule"},
+  {path: "**", redirectTo: ""}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Toolbar,
+    Main
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ContentModule,
+
+
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
